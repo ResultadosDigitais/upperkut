75fb69c1d362e8885bcf68b1e1a699068bf2606c
Nando Sousa <nandosousafr@gmail.com>
2018-06-29 17:38:23 -0300

    adds middleware

---
+++ lib/upperkut/middleware.rb
@@ -0,0 +1,34 @@
+module Upperkut
+  module Middleware
+    class Chain
+      attr_reader :items
+
+      def initialize
+        @items = []
+      end
+
+      def add(item)
+        return @items if @items.include?(item)
+        @items << item
+      end
+
+      def remove(item)
+        @items.delete(item)
+      end
+
+      def invoke(*args)
+        chain = @items.map(&:new)
+
+        traverse_chain = lambda do
+          if chain.empty?
+            yield
+          else
+            chain.shift.call(*args, &traverse_chain)
+          end
+        end
+
+        traverse_chain.call
+      end
+    end
+  end
+end
