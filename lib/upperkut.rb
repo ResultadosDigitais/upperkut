9e513fabc15e0076e6801983e38df72e2ae4641c
Thiago Chiarato <tchiarato@users.noreply.github.com>
2018-08-27 17:37:11 -0300

    Configuration ThreadSafe (#18)
    - Configuration wasn't thread safe due to a global configuration var
    - Remove Redis from configuration and pass it to the Strategy

--- lib/upperkut.rb
+++ lib/upperkut.rb
@@ -51,12 +51,11 @@ require 'redis'
 # 4) That's it :)
 module Upperkut
   class Configuration
-    attr_accessor :batch_size, :redis, :strategy, :max_wait, :polling_interval
+    attr_accessor :batch_size, :strategy, :max_wait, :polling_interval
 
     def self.default
       new.tap do |config|
         config.batch_size       = 1_000
-        config.redis            = Redis.new
         config.max_wait         = Integer(ENV['UPPERKUT_MAX_WAIT'] || 20)
         config.polling_interval = Integer(ENV['UPPERKUT_POLLING_INTERVAL'] || 5)
       end
--- spec/upperkut_spec.rb
+++ spec/upperkut_spec.rb
@@ -11,7 +11,6 @@ RSpec.describe Upperkut::Configuration do
       default = Upperkut::Configuration.default
 
       expect(default.batch_size).to eq 1_000
-      expect(default.redis). to be_instance_of(Redis)
       expect(default.max_wait).to eq 20
       expect(default.polling_interval).to eq 5
 
