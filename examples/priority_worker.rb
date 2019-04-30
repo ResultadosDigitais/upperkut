7b0e2ce86e4831ddea0f177ae644bdd2be90f715
Jean Gonçalves <jean.gnc@gmail.com>
2019-04-30 16:15:06 -0300

    Add sleep to demonstrate priority queue behavior

--- examples/priority_worker.rb
+++ examples/priority_worker.rb
@@ -8,13 +8,14 @@ class PriorityWorker
     config.strategy = Upperkut::Strategies::PriorityQueue.new(
       self,
       priority_key: -> { |item| item['tenant_id'] },
-      batch_size: 200
+      batch_size: 1
     )
   end
 
   def perform(items)
     items.each do |item|
       puts "event dispatched: #{item.inspect}"
+      sleep 1
     end
   end
 end
