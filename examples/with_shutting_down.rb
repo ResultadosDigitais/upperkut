691d0a24f839dc8ac2260992874fd73396d05fe7
candrebs <carlos.barbara@resultadosdigitais.com.br>
2019-04-30 13:05:34 -0300

    Fix rubocop issues

--- examples/with_shutting_down.rb
+++ examples/with_shutting_down.rb
@@ -1,18 +1,26 @@
 require_relative '../lib/upperkut/worker'
 
+# Upperkut::CLI.shutting_down? example
 class MyWorker
   include Upperkut::Worker
 
   def perform(items)
-    puts 'starting performing'
+    puts "starting performing on #{items}"
+    long_waiting
+  end
+
+  private
+
+  def long_waiting
     loop do
       puts 'hit ctrl+c to exit gracefully ...'
       sleep 3
-      if Upperkut::CLI.shutting_down?
-        puts 'upperkut is going to shutdown, terminating ...'
-        # return Upperkut::Shutdown exception to the upperkut enqueue the job
-        raise Upperkut::Shutdown
-      end
+
+      next unless Upperkut::CLI.shutting_down?
+
+      # return Upperkut::Shutdown exception to the upperkut enqueue the job
+      puts 'upperkut is going to shutdown, terminating ...'
+      raise Upperkut::Shutdown
     end
   end
 end
