4f6b9b8cbcc05ce4fac9910c08b3cd1cb46d4474
Jean Gonçalves <jean.gnc@gmail.com>
2020-03-24 22:50:33 -0300

    Add test coverage to #normalize_items

--- spec/upperkut/util_spec.rb
+++ spec/upperkut/util_spec.rb
@@ -4,12 +4,42 @@ module Upperkut
   RSpec.describe Util do
     include Upperkut::Util
 
+    describe '#normalize_items' do
+      it 'transforms hashes into items' do
+        items_hash = [{ 'my_property' => 1 }]
+        items = normalize_items(items_hash)
+
+        expect(items.map(&:body)).to eq(
+          [ 'my_property' => 1 ]
+        )
+      end
+
+      it 'knows how to handle a single item' do
+        items_hash = { 'my_property' => 1 }
+        items = normalize_items(items_hash)
+
+        expect(items.map(&:body)).to eq(
+          [ 'my_property' => 1 ]
+        )
+      end
+
+      it 'knows how to handle an Item class' do
+        items = Item.new('my_property' => 1)
+        normalized_items = normalize_items([ items ])
+
+        expect(normalized_items.map(&:body)).to eq(
+          [ 'my_property' => 1 ]
+        )
+      end
+    end
+
     describe '#decode_json_items' do
       context 'when collection has nil values' do
         it 'rejects nil and preserve collection' do
-          items = [nil, nil, nil, '{"id":2,"name":"value"}']
+          items_json = [nil, nil, nil, '{"body":{"id":2,"name":"value"}}']
+          items = decode_json_items(items_json).map(&:body)
 
-          expect(decode_json_items(items)).to eq(
+          expect(items).to eq(
             [
               'id' => 2,
               'name' => 'value'
