82f79a02c5dd94ca0e838a2f7b86f64ac5aa1d56
Jean Gonçalves <jean.gnc@gmail.com>
2020-04-07 17:35:03 -0300

    Improve spec maintainability

--- spec/upperkut/processor_spec.rb
+++ spec/upperkut/processor_spec.rb
@@ -3,20 +3,19 @@ require 'upperkut/processor'
 
 module Upperkut
   RSpec.describe Processor do
-    subject(:processor) { described_class }
+    subject(:processor) { described_class.new(worker, logger) }
 
-    class SmarterWorker
-      include Worker
-
-      def perform(_items); end
-
-      def handle_error(_exception, _items); end
-    end
+    let(:worker) { DummyWorker }
+    let(:logger) { Logger.new(nil) }
 
     class DummyWorker
       include Worker
 
       def perform(_items); end
+    end
+
+    class SmarterWorker < DummyWorker
+      def handle_error(_exception, _items); end
     end
 
     around do |example|
@@ -27,16 +26,15 @@ module Upperkut
       SmarterWorker.clear
     end
 
-    let(:worker) { DummyWorker }
-    let(:smarter_worker) { SmarterWorker }
-
     context 'when something goes wrong while fetching items' do
-      it 'it logs correctly' do
-        allow(DummyWorker).to receive(:fetch_items).and_raise(ArgumentError)
+      let(:logger) { spy('logger') }
 
-        logger = spy('logger')
-        execution = processor.new(worker, logger)
-        expect { execution.execute }.to raise_error(ArgumentError)
+      before do
+        allow(worker).to receive(:fetch_items).and_raise(ArgumentError)
+      end
+
+      it 'it logs correctly' do
+        expect { processor.execute }.to raise_error(ArgumentError)
 
         expect(logger).to have_received(:error) do |args|
           expect(args[:action]).to eq(:handle_execution_error)
@@ -51,43 +49,40 @@ module Upperkut
     end
 
     context 'when something goes wrong while processing' do
+      before do
+          allow_any_instance_of(worker).to receive(:perform).and_raise(ArgumentError)
+      end
+
       context 'when client implements handle_error method' do
+        let(:worker) { SmarterWorker }
+
         it 'calls .handle_error method' do
-          allow_any_instance_of(smarter_worker).to receive(:perform).and_raise(ArgumentError)
-
           item = { 'id' => '1', 'event' => 'open' }
-          smarter_worker.push_items(item)
+          worker.push_items(item)
 
-          execution = processor.new(smarter_worker)
-          expect_any_instance_of(smarter_worker).to receive(:handle_error)
+          expect_any_instance_of(worker).to receive(:handle_error)
 
-          expect { execution.execute }.not_to raise_error(ArgumentError)
+          expect { processor.execute }.not_to raise_error(ArgumentError)
         end
       end
 
       context 'when client doesnt implement handle_error method' do
         it 'requeue_item' do
-          allow_any_instance_of(worker).to receive(:perform).and_raise(ArgumentError)
-
           item = { 'id' => '1', 'event' => 'open' }
           worker.push_items(item)
 
           expect(worker.metrics['size']).to eq 1
 
-          execution = processor.new(worker)
-          expect { execution.execute }.to raise_error(ArgumentError)
+          expect { processor.execute }.to raise_error(ArgumentError)
           expect(worker.metrics['size']).to eq 1
         end
 
         it 'keeps the same latency' do
-          allow_any_instance_of(worker).to receive(:perform).and_raise(ArgumentError)
-
           item = Item.new({ 'id' => '1', 'event' => 'open' }, 2)
           worker.push_items(item)
 
           expect {
-            execution = processor.new(worker)
-            execution.execute rescue nil
+            processor.execute rescue nil
           }.not_to change { worker.metrics['latency'].to_i }
         end
       end
