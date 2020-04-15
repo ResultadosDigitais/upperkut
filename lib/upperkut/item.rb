d443851ed8a46e725d739dc5f1d060672eba2236
Jean Carlos Gonçalves <jean.gnc@gmail.com>
2020-04-15 14:50:43 -0300

    Fix items sorting (#63)
    * Bump pry from 0.13.0 to 0.13.1 (#60)
    
    Bumps [pry](https://github.com/pry/pry) from 0.13.0 to 0.13.1.
    - [Release notes](https://github.com/pry/pry/releases)
    - [Changelog](https://github.com/pry/pry/blob/master/CHANGELOG.md)
    - [Commits](https://github.com/pry/pry/compare/v0.13.0...v0.13.1)
    
    Signed-off-by: dependabot-preview[bot] <support@dependabot.com>
    
    Co-authored-by: dependabot-preview[bot] <27856297+dependabot-preview[bot]@users.noreply.github.com>
    
    * Ensure non-uniqueness on strategies that use sets as data structures
    
    * Fix lexicographically sorted items
    
    * Use plain old ruby syntax
    
    Co-authored-by: dependabot-preview[bot] <27856297+dependabot-preview[bot]@users.noreply.github.com>

--- lib/upperkut/item.rb
+++ lib/upperkut/item.rb
@@ -1,11 +1,14 @@
+require 'securerandom'
+
 module Upperkut
   class Item
-    attr_reader :enqueued_at
+    attr_reader :id, :body, :enqueued_at
 
-    def initialize(body, enqueued_at = nil)
+    def initialize(body:, id: nil, enqueued_at: nil)
       raise ArgumentError, 'Body should be a Hash' unless body.is_a?(Hash)
 
       @body = body
+      @id = id || SecureRandom.uuid
       @enqueued_at = enqueued_at || Time.now.utc.to_i
     end
 
@@ -21,12 +24,9 @@ module Upperkut
       @body.key?(key)
     end
 
-    def body
-      @body
-    end
-
     def to_json
       JSON.generate(
+        'id' => @id,
         'body' => @body,
         'enqueued_at' => @enqueued_at
       )
@@ -34,7 +34,8 @@ module Upperkut
 
     def self.from_json(item_json)
       hash = JSON.parse(item_json)
-      new(hash['body'], hash['enqueued_at'])
+      id, body, enqueued_at = hash.values_at('id', 'body', 'enqueued_at')
+      new(id: id, body: body, enqueued_at: enqueued_at)
     end
   end
 end
